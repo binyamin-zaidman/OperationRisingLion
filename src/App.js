576f89a07049fd9a225a14a5b0a725230191b8d7
binyamin-zaidman <bl8843@gmail.com>
2025-06-19 14:49:11 +0300

    creat components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
+import HelloUser from "./Components/HelloUser";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p id="helloMan">
-          Hello ______
-        </p>
-        <input type="text" placeholder="Enter your name" id="name"/>
-      <button type="button" id="button" onClick={() => {document.getElementById("helloMan").innerHTML = "Hello " + document.getElementById("name").value; document.getElementById("name").value = "";}}>Enter</button>
-      </header>
+      <HelloUser />
+      <HelloUser />
+      <HelloUser />
     </div>
   );
 }
